6fef8750528948da8814af7a5f03fef3481b5cd3
Song Qi <songqi9134@163.com>
2019-12-19 19:30:26 +0800

    init blog

--- push_source.sh
+++ push_source.sh
@@ -2,5 +2,5 @@ git init
 git checkout -b source
 git add -A
 git commit -m "init blog"
-git remote add origin git@github.com:libmelo/libmelo.github.io
+git remote add origin git@github.com:libmelo/libmelo.github.io.git
 git push origin source
