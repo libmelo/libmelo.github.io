5cc7d5551fed462db1841447d161fc176395b27f
Song Qi <songqi9134@163.com>
2019-12-20 13:51:41 +0800

    update1220

--- push_source.sh
+++ push_source.sh
@@ -1,6 +1,6 @@
 git init
 git checkout source
 git add -A
-git commit -m "update"
+git commit -m "update1220"
 # git remote add origin git@github.com:libmelo/libmelo.github.io.git
 git push origin source
